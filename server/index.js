9af061e274a2fe2f84747020aa8411f91016215f
Adrian-Walker <awalker3365@gmail.com>
2022-07-01 20:22:33 -0400

    server side updated

--- server/index.js
+++ server/index.js
@@ -3,5 +3,8 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-const express = require("express");
 const app = express();
+
+app.use(bodyParser.json({ limit: "32mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }));
+app.use(cors())
