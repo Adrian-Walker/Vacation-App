4a982be33c7c12e4988ae240c263080e5b72799a
Adrian-Walker <awalker3365@gmail.com>
2022-07-03 09:28:58 -0400

    added mongoose connection

--- server/index.js
+++ server/index.js
@@ -7,4 +7,15 @@ const app = express();
 
 app.use(bodyParser.json({ limit: "32mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }));
-app.use(cors())
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://1wayoranother:raymond@cluster0.wrqoaae.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() => {
+    console.log(`Server running on port ${PORT}`);
+  })
+  .catch((err) => console.log(err.message));
