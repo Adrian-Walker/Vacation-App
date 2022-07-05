6fba23fdf7b4ffb2df170b21f43fcd1a2ab08e6d
Adrian-Walker <awalker3365@gmail.com>
2022-07-05 10:52:15 -0400

    frontend updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 function App() {
-  return <div className="App">
-    <h1>App</h1>
-  </div>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar positiion="static" color="inherit">
+        <Typography variant="h2" align="center">
+          My Vacations
+        </Typography>
+      </AppBar>
+    </Container>
+  );
 }
 
 export default App;
