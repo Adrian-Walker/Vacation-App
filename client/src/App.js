c11b18b0c5f1d0e9fa020c94cd80146502585b7a
Adrian-Walker <awalker3365@gmail.com>
2022-07-05 11:33:07 -0400

    added material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import hawaii from "./images/hawaii.jpeg";
 
 function App() {
   return (
@@ -8,7 +9,18 @@ function App() {
         <Typography variant="h2" align="center">
           My Vacations
         </Typography>
+        <img src={hawaii} alt="my-vacations" height="100"></img>
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={4}
+          ></Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 }
