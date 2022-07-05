cdfecebaea61450250d2c8612c4a6cee04466141
Adrian-Walker <awalker3365@gmail.com>
2022-07-04 21:04:30 -0400

    post.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,5 +1,12 @@
-export const getPosts = (req, res) => {
-  res.send("Post Works");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await PostMessage.find();
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {}
 };
 
 export const createPost = (req, res) => {
