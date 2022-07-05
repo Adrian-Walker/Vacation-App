509eb2e7037668eb6d432715fdd7e1f283a4cae7
Adrian-Walker <awalker3365@gmail.com>
2022-07-05 14:34:12 -0400

    styles

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,25 @@ import React from "react";
 import Posts from "./components/Posts/Posts.js";
 import Form from "./components/Form/Form.js";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import usestyles from "./styles";
+
 import hawaii from "./images/hawaii.jpeg";
 
 function App() {
+  const classes = usestyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar positiion="static" color="inherit">
-        <Typography variant="h2" align="center">
+      <AppBar className={classes.appBar} positiion="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           My Vacations
         </Typography>
-        <img src={hawaii} alt="my-vacations" height="60"></img>
+        <img
+          className={classes.image}
+          src={hawaii}
+          alt="my-vacations"
+          height="70"
+        ></img>
       </AppBar>
       <Grow in>
         <Container>
