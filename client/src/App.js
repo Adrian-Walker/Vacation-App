64e618fcdc015f84a41fbf47bd0b838b75fbede2
Adrian-Walker <awalker3365@gmail.com>
2022-07-05 12:01:20 -0400

    updated components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Posts from "./components/Posts/Posts.js";
+import Form from "./components/Form/Form.js";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import hawaii from "./images/hawaii.jpeg";
 
@@ -18,7 +20,15 @@ function App() {
             justify="space-between"
             alignItems="stretch"
             spacing={4}
-          ></Grid>
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
         </Container>
       </Grow>
     </Container>
